ba1164baa8f82e6d906cb59b8aac3d84f46a1d3c
n18011 <n18011@std.it-college.ac.jp>
2019-10-18 14:42:47 +0900

    funciton-simplizing

--- functions/index.js
+++ functions/index.js
@@ -2,32 +2,12 @@ const functions = require('firebase-functions')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-const admin = require('firebase-admin')
-require('firebase/firestore')
-require('firebase/auth')
 
-var firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: 'graduation-task-d7fc3.firebaseapp.com',
-  databaseURL: 'https://graduation-task-d7fc3.firebaseio.com',
-  projectId: 'graduation-task-d7fc3',
-  storageBucket: 'graduation-task-d7fc3.appspot.com',
-  messagingSenderId: '755416615184',
-  appId: '1:755416615184:web:4105f566a4aaf89ab3fd95'
-}
-// Initialize Firebase
-admin.initializeApp(firebaseConfig)
-const db = admin.firestore()
-
-// admin.initializeApp(functions.config().firebase)
-/*
 const challonge = require('challonge')
 const client = challonge.createClient({
   apiKey: 'THQwE1NobDxeWTRbAb8ACEtrUV4jDse7C6N7PwvU'
 })
-*/
 const express = require('express')
-const request = require('superagent')
 const cors = require('cors')
 
 const REGION_TOKYO = 'asia-northeast1'
@@ -47,7 +27,21 @@ const corsConfig = {
 // app.use(express.json())
 app.use(cors(corsConfig))
 
-app.get('/api', async (req, res) => {
+app.get('/', (req, res) => {
+  const events = []
+  client.tournaments.show({
+    id: 'n18011test',
+    callback: (err, data) => {
+      if (err) {
+        console.log(err)
+      }
+      events.push(data)
+    }
+  })
+  res.json(events)
+})
+/*
+app.get('/', async (req, res) => {
   const date = new Date()
   const hours = (date.getHours() % 12) + 1 // London is UTC + 1hr;
   await res.json({ bongs: 'BONG '.repeat(hours) })
@@ -77,8 +71,9 @@ app.get('/api/events', async (req, res) => {
   res.json(events)
 }
 )
+*/
 
-exports.bigben = functions.region(REGION_TOKYO).https.onRequest(app)
+exports.api = functions.region(REGION_TOKYO).https.onRequest(app)
 
 /*
 exports.helloWorld = functions.https.onRequest((request, response) => {
