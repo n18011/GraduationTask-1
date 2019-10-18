0767b5ea0c60afbbe73140695be6780b75605909
n18011 <n18011@std.it-college.ac.jp>
2019-10-18 14:28:44 +0900

    change views/Events.js change views/NotHold.js

--- functions/index.js
+++ functions/index.js
@@ -30,40 +30,55 @@ const express = require('express')
 const request = require('superagent')
 const cors = require('cors')
 
+const REGION_TOKYO = 'asia-northeast1'
 const app = express()
 
-app.use(cors({ origin: true }))
+const whitelist = ['https://graduation-task-d7fc3.web.app', 'https://graduation-task-d7fc3.firebaseapp.com']
 
-app.get('/api', (req, res) => {
+const corsConfig = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+// app.use(express.json())
+app.use(cors(corsConfig))
+
+app.get('/api', async (req, res) => {
   const date = new Date()
   const hours = (date.getHours() % 12) + 1 // London is UTC + 1hr;
-  res.json({ bongs: 'BONG '.repeat(hours) })
+  await res.json({ bongs: 'BONG '.repeat(hours) })
 })
 
-app.get('/api/show', (req, res) => {
-  request.get('https://n18011:THQwE1NobDxeWTRbAb8ACEtrUV4jDse7C6N7PwvU@api.challonge.com/v1/tournaments.json')
+app.get('/api/show', async (req, res) => {
+  await request.get('https://n18011:THQwE1NobDxeWTRbAb8ACEtrUV4jDse7C6N7PwvU@api.challonge.com/v1/tournaments.json')
     .accept('application/json')
     .end((err, resp) => {
-      res.json(resp)
+      res.json(resp.body)
     })
 })
 
-app.get('/api/events', (req, res) => {
-  db.collection('events').where('status.nowhold', '==', true).get()
+app.get('/api/events', async (req, res) => {
+  const events = []
+  await db.collection('events').where('status.nowhold', '==', true).get()
     .then(snapshot => {
       snapshot.forEach(doc => {
         console.log(doc.id, '=>', doc.data())
-        res.json(
+        events.push(
           {
             ...doc.data(),
             id: doc.id
           })
       })
     })
+  res.json(events)
 }
 )
 
-exports.bigben = functions.https.onRequest(app)
+exports.bigben = functions.region(REGION_TOKYO).https.onRequest(app)
 
 /*
 exports.helloWorld = functions.https.onRequest((request, response) => {
--- src/views/NotHold.js
+++ src/views/NotHold.js
@@ -1,29 +1,27 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useMemo } from 'react'
+
 import db from '../Firebase'
+
 import EventsCard from '../components/EventsCard'
 
 export default () => {
-    const [events, setEvents] = useState([])
-    useEffect(() => {
-      const events = []
-      let eventsRef = db.collection('events');
-        eventsRef.where('status.willhold', '==', true).get()
-            .then(snapshot => {
-                snapshot.forEach(doc => {
-                console.log(doc.id, '=>', doc.data());
-                events.push({
-                    ...doc.data(),
-                    id: doc.id})
-                });
-            })
-            .catch(err => {
-                console.log('Error getting documents', err);
-            })
-        setEvents(events)
-        },[])
-    return (
-        <div>
-            <EventsCard cards={events}></EventsCard>
-        </div>
+  const [events, setEvents] = useState([])
+  const collection = useMemo(() => {
+    const col = db.collection('events')
+
+    // 更新イベント監視
+    col.where('status.willhold', '==', true).onSnapshot(query => {
+      const data = []
+      query.forEach(doc => data.push({ ...doc.data(), docId: doc.id }))
+      setEvents(data)
+    })
+
+    return col
+  }, [])
+
+  return (
+    <>
+      <EventsCard cards={events} />
+    </>
   )
 }
