6d31d6321f9d4b0b1b1e66b931072c2b374666a5
n18011 <n18011@std.it-college.ac.jp>
2019-10-20 01:57:56 +0900

    .gitignore => functions/以下Firebase.js and challonge-client.jsを追加,firebase.json => rewriteを修正, functions/index.js => 機能を追加(challongeAPIno)

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,21 @@
-const functions = require('firebase-functions')
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-const challonge = require('challonge')
-const client = challonge.createClient({
-  apiKey: 'THQwE1NobDxeWTRbAb8ACEtrUV4jDse7C6N7PwvU'
-})
+const functions = require('firebase-functions')
+
 const express = require('express')
+const app = express()
+const REGION_TOKYO = 'asia-northeast1' // functionのリージョンhostingと同じ
+
 const cors = require('cors')
 
-const REGION_TOKYO = 'asia-northeast1'
-const app = express()
-
-const whitelist = ['https://graduation-task-d7fc3.web.app', 'https://graduation-task-d7fc3.firebaseapp.com']
+const client = require('./challonge-client.js')
+/*
+const whitelist = [ // functionsとの通信を許可するドメインリスト
+  'https://graduation-task-d7fc3.web.app',
+  'https://graduation-task-d7fc3.firebaseapp.com',
+  'https://asia-northeast1-graduation-task-d7fc3.cloudfunctions.net'
+]
 
 const corsConfig = {
   origin: function (origin, callback) {
@@ -24,82 +26,327 @@ const corsConfig = {
     }
   }
 }
-// app.use(express.json())
-app.use(cors(corsConfig))
+app.use(cors(corsConfig)) // 制限付きcors
+*/
+app.use(cors({})) // 全許可cors
 
-app.get('/', (req, res) => {
-  const events = []
-  client.tournaments.show({
-    id: 'n18011test',
+/*
+ * Tonamentsのmethod一覧
+ */
+
+app.route('/tournaments')
+// トーナメント一覧を参照
+  .get(async (req, res) => {
+    await client.tournaments.index({
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+          return err
+        }
+        res.json(data)
+      }
+    })
+  })
+
+// 新規トーナメントを作成
+  .post(async (req, res) => {
+    const tournament = req.body.tournament
+    await client.tournaments.create({
+      tournament,
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+          return err
+        }
+        res.json(data)
+      }
+    })
+  })
+
+app.route('/tournaments/:tournament')
+// :tournamentのトーナメントを参照(:tournamentはtournament.url)
+  .get(async (req, res) => {
+    const id = req.params.tournament
+    await client.tournaments.show({
+      id,
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        res.json(data)
+      }
+    })
+  })
+
+// :tournamentのトーナメント情報の更新(:tournamentはtournament.url)*req.bodyにobjが必要
+  .put(async (req, res) => {
+    const id = req.params.tournament
+    const tournament = req.body.tournament
+    await client.tournaments.update({
+      id,
+      tournament,
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        res.json(data)
+      }
+    })
+  })
+
+// :tournamentのトーナメントを削除(:tournamentはtournament.url)
+  .delete(async (req, res) => {
+    const id = req.params.tournament
+    await client.tournaments.destroy({
+      id,
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        res.json(data)
+      }
+    })
+  })
+
+// :tournamentのトーナメントをスタート
+app.post('/tournaments/:tournament/start', async (req, res) => {
+  const id = req.params.tournament
+  await client.tournaments.start({
+    id,
     callback: (err, data) => {
       if (err) {
         console.log(err)
       }
-      events.push(data)
+      res.json(data)
     }
   })
-  res.json(events)
+})
+
+// :tournamentのトーナメントを終了
+app.post('/tournaments/:tournament/finalize', async (req, res) => {
+  const id = req.params.tournament
+  await client.tournaments.finalize({
+    id,
+    callback: (err, data) => {
+      if (err) {
+        console.log(err)
+      }
+      res.json(data)
+    }
+  })
+})
+
+// :tournamentのトーナメントをリセット
+app.post('/tournaments/:tournament/reset', async (req, res) => {
+  const id = req.params.tournament
+  await client.tournaments.reset({
+    id,
+    callback: (err, data) => {
+      if (err) {
+        console.log(err)
+      }
+      res.json(data)
+    }
+  })
+})
+
+// :tournamentのトーナメントを開始する?
+app.post('/tournaments/:tournament/process_check_ins', async (req, res) => {
+  const id = req.params.tournament
+  await client.tournaments.processCheckIns({
+    id,
+    callback: (err, data) => {
+      if (err) {
+        console.log(err)
+      }
+      res.json(data)
+    }
+  })
+})
+
+// :tournamentのトーナメントを中止する?
+app.post('/tournaments/:tournament/abort_check_in', async (req, res) => {
+  const id = req.params.tournament
+  await client.tournaments.abortCheckIns({
+    id,
+    callback: (err, data) => {
+      if (err) {
+        console.log(err)
+      }
+      res.json(data)
+    }
+  })
+})
+
+/*
+ * Matchesのmethod一覧
+ */
+
+// :tournament中の試合一覧を参照
+app.get('/tournaments/:tournament/matches', async (req, res) => {
+  const id = req.params.tournament
+  await client.matches.index({
+    id,
+    callback: (err, data) => {
+      if (err) {
+        console.log(err)
+      }
+      res.json(data)
+    }
+  })
+})
+
+app.route('/tournaments/:tournament/matches/:match_id')
+// :tournament中の試合詳細を参照
+  .get(async (req, res) => {
+    const id = req.params.tournament
+    const matchId = req.params.match_id
+    await client.matches.show({
+      id,
+      matchId,
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        res.json(data)
+      }
+    })
+  })
+
+// :tournament中の試合詳細を更新
+  .put(async (req, res) => {
+    const id = req.params.tournament
+    const matchId = req.params.match_id
+    const match = req.body.match
+    await client.matches.update({
+      id,
+      matchId,
+      match,
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        res.json(data)
+      }
+    })
+  })
+
+/*
+ * Participantsのmethod一覧
+ */
+
+app.route('/tournaments/:tournament/participants')
+// :tournament中のプレイヤー一覧を参照
+  .get(async (req, res) => {
+    const id = req.params.tournament
+    await client.participants.index({
+      id,
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        res.json(data)
+      }
+    })
+  })
+
+// :tournament中のプレイヤーを新規作成
+  .post(async (req, res) => {
+    const id = req.params.tournament
+    const participant = req.body.participant
+    await client.participants.create({
+      id,
+      participant,
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        res.json(data)
+      }
+    })
+  })
+
+app.route('/tournaments/:tournament/participants/:participant_id')
+// :tournament中のプレイヤー詳細を参照
+  .get(async (req, res) => {
+    const id = req.params.tournament
+    const participantId = req.params.participant_id
+    await client.participants.show({
+      id,
+      participantId,
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        res.json(data)
+      }
+    })
+  })
+
+// :tournament中のプレイヤー詳細を更新
+  .put(async (req, res) => {
+    const id = req.params.tournament
+    const participantId = req.params.participant_id
+    const participant = req.body.participant
+    await client.participants.update({
+      id,
+      participantId,
+      participant,
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        res.json(data)
+      }
+    })
+  })
+
+// :tournament中のプレイヤー詳細を削除
+  .delete(async (req, res) => {
+    const id = req.params.tournament
+    const participantId = req.params.participant_id
+    await client.participants.destroy({
+      id,
+      participantId,
+      callback: (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        res.json(data)
+      }
+    })
+  })
+
+// :tournament中のプレイヤーをランダムに割り振る?
+app.get('/tournaments/:tournament/participants/randomize', async (req, res) => {
+  const id = req.params.tournament
+  await client.participants.randomize({
+    id,
+    callback: (err, data) => {
+      if (err) {
+        console.log(err)
+      }
+      res.json(data)
+    }
+  })
 })
 /*
+// トーナメント一覧を参照
+app.get('/tournaments', async (req, res) => {
+  await client.tournaments.index({
+    callback: (err, data) => {
+      if (err) {
+        console.log(err)
+      }
+      res.json(data)
+    }
+  })
+})
 app.get('/', async (req, res) => {
   const date = new Date()
   const hours = (date.getHours() % 12) + 1 // London is UTC + 1hr;
   await res.json({ bongs: 'BONG '.repeat(hours) })
 })
-
-app.get('/api/show', async (req, res) => {
-  await request.get('https://n18011:THQwE1NobDxeWTRbAb8ACEtrUV4jDse7C6N7PwvU@api.challonge.com/v1/tournaments.json')
-    .accept('application/json')
-    .end((err, resp) => {
-      res.json(resp.body)
-    })
-})
-
-app.get('/api/events', async (req, res) => {
-  const events = []
-  await db.collection('events').where('status.nowhold', '==', true).get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-        console.log(doc.id, '=>', doc.data())
-        events.push(
-          {
-            ...doc.data(),
-            id: doc.id
-          })
-      })
-    })
-  res.json(events)
-}
-)
 */
 
 exports.api = functions.region(REGION_TOKYO).https.onRequest(app)
-
-/*
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('helloWorld!')
-})
-
-exports.events = functions.https.onRequest(async (req, res) => {
-  await client.tournaments.show({
-    id: 'n18011test',
-    callback: (err, data) => {
-      console.log(err)
-      res.send(data)
-    }
-  })
-})
-
-exports.tournamentsCreate = functions.https.onRequest((req, res) => {
-  client.tournaments.create({
-    tournament: {
-      name: 'functionTest',
-      url: 'functionTest',
-      tournamentType: 'single elimination'
-    },
-    callback: (err, data) => {
-      console.log(err, data)
-    }
-  })
-})
-*/
--- src/views/Top.js
+++ src/views/Top.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import request from 'superagent'
 
 export default () => {
+  useMemo(() => {
+    const post = async () => {
+      await request.post('https://asia-northeast1-graduation-task-d7fc3.cloudfunctions.net/api/tournaments')
+        .set('Content-Type', 'application/json')
+        .send({
+          tournament: {
+            name: 'n18011no1',
+            url: 'n18011no1',
+            tournamentType: 'single elimination'
+          }
+        })
+        .end((err, res) => {
+          console.log(err)
+          console.log(res)
+        })
+    }
+    post()
+  }, [])
   return (
     <>
       <h1>Hello, Top</h1>
