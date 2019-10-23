d87f681fc7c61bc13fa9fba65abce29914ceb9d3
n18011 <n18011@std.it-college.ac.jp>
2019-10-23 16:31:33 +0900

    fdasd

--- src/views/Top.js
+++ src/views/Top.js
@@ -1,37 +1,37 @@
 import React, { useState, useMemo } from 'react'
-import {db} from '../Firebase'
+import { db } from '../Firebase'
 import EventsCard from '../components/EventsCard'
 import EventsListWill from '../components/EventsListWill'
 
 export default () => {
-    const [willevents, setWevents] = useState([])
-    const [heldevents, setHevents] = useState([])
+  const [willevents, setWevents] = useState([])
+  const [heldevents, setHevents] = useState([])
 
-    useMemo(() => {
-      const col = db.collection('events')
-            
-        col.where('status.willhold', '==', true).onSnapshot(query => {
-         const data =[]
-         query.forEach(doc => data.push({ ...doc.data(), id: doc.id}))
-                    setWevents(data)
-                })
-                return col
-            }, [])
+  useMemo(() => {
+    const col = db.collection('events')
 
-    useMemo(() => {
-      const col = db.collection('events')
-    
-        col.where('status.held', '==', true).onSnapshot(query => {
-            const data = []
-            query.forEach(doc => data.push({id: doc.id}))
-            setHevents(data)
-        })
-    }, [])
+    col.where('status.willhold', '==', true).onSnapshot(query => {
+      const data = []
+      query.forEach(doc => data.push({ ...doc.data(), id: doc.id }))
+      setWevents(data)
+    })
+    return col
+  }, [])
+
+  useMemo(() => {
+    const col = db.collection('events')
+
+    col.where('status.held', '==', true).onSnapshot(query => {
+      const data = []
+      query.forEach(doc => data.push({ id: doc.id }))
+      setHevents(data)
+    })
+  }, [])
 
   return (
       <>
-          <EventsCard cards={willevents} />
-          <EventsListWill cards={heldevents} />
+        <EventsCard cards={willevents} />
+        <EventsListWill cards={heldevents} />
     </>
   )
 }
