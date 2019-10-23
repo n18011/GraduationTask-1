ceae6a947468f4606a79854a70dd66abc8494267
n18011 <n18011@std.it-college.ac.jp>
2019-10-23 16:23:21 +0900

    change Grid to main.layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { useState, useContext } from 'react'
+import React from 'react'
 
 import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
-
-import {
-  Grid
-} from '@material-ui/core'
 
 import { AuthContext, AuthProvider } from './components/Auth'
 import { makeStyles } from '@material-ui/core/styles'
@@ -46,28 +42,26 @@ export default () => {
   const classes = useStyles()
   return (
     <AuthProvider>
-      <Grid container justify='center'>
-        <Router>
-          <Grid items spacing={3} justify='center' alignItem='center' md={8} xs>
-            <Header />
-            <Route exact path='/' component={Top} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/divide' component={Divide} />
-            <Route exact path='/events' component={Events} />
-            <Route exact path='/events/:eid/matchs/:mid' component={Result} />
-            <Route exact path='/held' component={Held} />
-            <Route exact path='/player/:pid' component={PlayerHome} />
-            <Route exact path='/nothold' component={NotHold} />
-            <Route exact path='/player/:pid/apply' component={PlayerApply} />
-            <Route exact path='/player/:pid/join' component={Join} />
-            <Route exact path='/events/:eid/matchs/:mid/users/:uid' component={PlayerResult} />
-            <Route exact path='/player/:pid/delete' component={PlayerDelete} />
-            <Route exact path='/admin/:aid' component={AdminHome} />
-            <Route exact path='/admin/:aid/input' component={AdminApply} />
-            <Route exact path='/admin/:aid/change' component={AdminChange} />
-          </Grid>
-        </Router>
-      </Grid>
+      <Router>
+        <Header />
+        <main className={classes.layout}>
+          <Route exact path='/' component={Top} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/divide' component={Divide} />
+          <Route exact path='/events' component={Events} />
+          <Route exact path='/events/:eid/matchs/:mid' component={Result} />
+          <Route exact path='/held' component={Held} />
+          <Route exact path='/player/:pid' component={PlayerHome} />
+          <Route exact path='/nothold' component={NotHold} />
+          <Route exact path='/player/:pid/apply' component={PlayerApply} />
+          <Route exact path='/player/:pid/join' component={Join} />
+          <Route exact path='/events/:eid/matchs/:mid/users/:uid' component={PlayerResult} />
+          <Route exact path='/player/:pid/delete' component={PlayerDelete} />
+          <Route exact path='/admin/:aid' component={AdminHome} />
+          <Route exact path='/admin/:aid/input' component={AdminApply} />
+          <Route exact path='/admin/:aid/change' component={AdminChange} />
+        </main>
+      </Router>
 
     </AuthProvider>
   )
