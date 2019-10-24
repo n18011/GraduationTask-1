a3d5c6a681016dde2751482e2ce317b56aa404fe
n18011 <n18011@std.it-college.ac.jp>
2019-10-24 12:15:18 +0900

    changing Result.js reletion

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -2,46 +2,94 @@
 import React from 'react'
 
 import {
-  Card,
-  CardHeader,
-  CardContent,
+  Grid,
   Typography,
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
-  TableHead,
   List,
   ListItem,
   ListItemText,
   Paper
 } from '@material-ui/core'
 
-export default () => {
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    width: 300,
+    height: 300,
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 200
+    }
+  },
+  list: {
+    width: 200,
+    height: 45
+  },
+  text: {
+  }
+}))
+
+export default ({ children }) => {
+  const classes = useStyles()
+
   const products = [
     {
-      name: 'asdfa',
-      desc: 'asdfb',
-      price: 'asdfc'
+      player1: '11',
+      player2: '8'
+    },
+    {
+      player1: '11',
+      player2: '8'
+    },
+    {
+      player1: '8',
+      player2: '11'
+    },
+    {
+      player1: '11',
+      player2: '13'
+    },
+    {
+      player1: '11',
+      player2: '8'
     }
   ]
   return (
     <>
-      <Paper>
 
-        <List>
-
-          {products.map(product => (
-            <ListItem key={product.name}>
-              <ListItemText primary={product.name} secondary={product.desc} />
-              <Typography variant='body2'>{product.price}</Typography>
-            </ListItem>
-          ))}
-          <Typography variant='subtitle1' align='center'>
-            $34.06
-          </Typography>
-        </List>
-
+      <Paper className={classes.paper}>
+        <Typography variant='h5' align='center'>(プレイヤー名)</Typography>
+        {products.map((product, index) => {
+          return (
+            <>
+              <List className={classes.list}>
+                <Grid item xs>
+                  <ListItem key={Object.keys(product)[0]} alignItem='center'>
+                    <ListItemText secondary={index + 1} />
+                    <Typography className={classes.text} variant='h6'>{product.player1}</Typography>
+                  </ListItem>
+                </Grid>
+              </List>
+            </>
+          )
+        })}
+        <Typography variant='h5' align='center'>3(結果)</Typography>
+      </Paper>
+      {children}
+      <Paper className={classes.paper}>
+        {products.map((product, index) => {
+          return (
+            <>
+              <List className={classes.list}>
+                <Grid item xs>
+                  <ListItem key={Object.keys(product)[1]}>
+                    <ListItemText secondary={index + 1} />
+                    <Typography variant='body2'>{product.player2}</Typography>
+                  </ListItem>
+                </Grid>
+              </List>
+            </>
+          )
+        })}
       </Paper>
     </>
   )
--- src/views/Result.js
+++ src/views/Result.js
@@ -2,15 +2,9 @@ import React from 'react'
 
 import {
   Grid,
-  Card,
-  CardHeader,
-  CardContent,
+  Paper,
   Typography,
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
-  TableHead
+  TextField
 } from '@material-ui/core'
 
 import { makeStyles } from '@material-ui/core/styles'
@@ -26,30 +20,99 @@ const useStyles = makeStyles(theme => ({
 // VS以外の全てのTypography部にデータが入る
 export default () => {
   const classes = useStyles()
+  const [values, setValues] = React.useState({
+    name: 'Cat in the Hat',
+    age: '',
+    set1: 5,
+    set2: 0,
+    set3: 0,
+    set4: 0,
+    set5: null,
+    multiline: 'Controlled',
+    currency: 'EUR'
+  })
+  const products = [
+    {
+      player1: '11',
+      player2: '8'
+    },
+    {
+      player1: '11',
+      player2: '8'
+    },
+    {
+      player1: '8',
+      player2: '11'
+    },
+    {
+      player1: '11',
+      player2: '13'
+    },
+    {
+      player1: '11',
+      player2: '8'
+    }
+  ]
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value })
+  }
   return (
     <>
       <Grid container alignItems='center' justify='center'>
 
-        <Grid item xs>
-          <ResultCard />
+        <Grid item xs container direction='column'>
+          <Paper>
+
+            <Grid item xs>
+              <Typography variant='h5' align='center'>
+                Player1
+              </Typography>
+            </Grid>
+
+            <Grid item container>
+              {products.map((product, index) => (
+                <>
+
+                  <Grid item xs={6}>
+                    <typography variant='body1' gutterBottom>{index + 1}</typography>
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <TextField
+                      id='filled-number'
+                      label={index + 1}
+                      defaultvalue={`values.set${index + 1}`}
+                      onChange={handleChange(`set${index + 1}`)}
+                      type='number'
+                      className={classes.textField}
+                      InputLabelProps={{
+                        shrink: true
+                      }}
+                      margin='normal'
+                      variant='filled'
+                    />
+                  </Grid>
+                </>
+              ))
+              }
+            </Grid>
+
+            <Grid item xs>
+              <Typography variant='body1' align='center'>結果</Typography>
+            </Grid>
+
+          </Paper>
         </Grid>
 
-        <Grid item xs={2}>
+        <Grid item xs>
           <Typography variant='h4' align='center'>VS</Typography>
         </Grid>
 
         <Grid item xs>
-          <Card>
-            <CardHeader title={'head'} align='center' />
-            <CardContent>
-              <Typography>body1</Typography>
-              <Typography>body2</Typography>
-              <Typography>body3</Typography>
-            </CardContent>
-            <CardContent>
-              <Typography align='center'>foot</Typography>
-            </CardContent>
-          </Card>
+          <Paper>
+            <Typography variant='h4'>fasdfasd</Typography>
+          </Paper>
         </Grid>
 
       </Grid>
