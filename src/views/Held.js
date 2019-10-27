ae930c81ae7b2b093db7dc909f74d7919872f0e9
n18008 <n18008@std.it-college.ac.jp>
2019-10-27 18:14:42 +0900

    Held.js fini

--- src/views/Held.js
+++ src/views/Held.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
+import { db } from '../Firebase'
 
 import {
   Typography
@@ -7,10 +8,21 @@ import {
 import EventsListWill from '../components/EventsListWill' // propsに開催済み大会のデータ入力
 
 export default () => {
+  const [heldevents, setHevents] = useState([])
+  useMemo(() => {
+    const col = db.collection('events')
+  
+    col.where('status.held', '==', true).onSnapshot(query => {
+      const data =[]
+      query.forEach(doc => data.push({ ...doc.data(), id:doc.id }))
+      setHevents(data)
+    })
+  }, [])
+
   return (
     <>
       <Typography variant='h4'>過去の大会</Typography>
-      <EventsListWill />
+      <EventsListWill cards={heldevents} />
     </>
   )
 }
