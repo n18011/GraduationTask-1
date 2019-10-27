48c68c94dbf1e8d7cd18c1526cee65182cb93f3d
n18011 <n18011@std.it-college.ac.jp>
2019-10-27 16:23:56 +0900

    add menu icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  Link
+} from 'react-router-dom'
 
 import {
   AppBar,
   Toolbar,
   Typography,
-  CssBaseline
+  CssBaseline,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Divider,
+  Button,
+  Drawer,
+  IconButton
 } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const useStyles = makeStyles(theme => ({
+
+}))
 
 export default () => {
+  const classes = useStyles()
+  const [state, setState] = useState({
+    left: false
+  })
+
+  const toggleDrawer = (side, open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return
+    }
+
+    setState({ ...state, [side]: open })
+  }
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role='presentation'
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? 'inbox' : 'mail'}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )
   return (
     <>
       <AppBar position='relative'>
         <Toolbar>
+          <IconButton
+            color='inherit'
+            aria-label='menu'
+            edge='start'
+            onClick={toggleDrawer('left', true)}
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography variant='h5' color='inherit' noWrap>
             大会運営アプリ
           </Typography>
         </Toolbar>
       </AppBar>
       <CssBaseline />
+      <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
+        {sideList('left')}
+      </Drawer>
       <ul>
         <li><Link to='/'>Top</Link></li>
         <li><Link to='/login'>login</Link></li>
