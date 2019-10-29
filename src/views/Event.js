257782a82d1811667a8a4607bdbb08062945022a
n18008 <n18008@std.it-college.ac.jp>
2019-10-30 02:32:18 +0900

    I won to Event.js re

--- src/views/Event.js
+++ src/views/Event.js
@@ -35,76 +35,76 @@ export default ({ match }) => {
       const main = async () => {
 
         let func1 = () => {request.get(url).end((err, res) => {
-        P1[res.body.participant.id] = res.body.participant.name
-        const keyarray = Object.keys(P1)
-        var j = 0
-        if (keyarray.length === matchLen) {
-          while(matchLen > j) {
-            var n = 0
-            while(matchLen > n) {
-              console.log(infomation[j].player1Id)
-              console.log(keyarray[n])
-              if (infomation[j].player1Id.toString() === keyarray[n]) {
-                console.log('OK')
-                infomation[j].player1Name = P1[keyarray[n]]
-                break
+          P1[res.body.participant.id] = res.body.participant.name
+          const keyarray = Object.keys(P1)
+          var j = 0
+          if (keyarray.length === matchLen) {
+            while(matchLen > j) {
+              var n = 0
+              while(matchLen > n) {
+                console.log(infomation[j].player1Id)
+                console.log(keyarray[n])
+                if (infomation[j].player1Id.toString() === keyarray[n]) {
+                  console.log('OK')
+                  infomation[j].player1Name = P1[keyarray[n]]
+                  break
+                }
+                else {
+                  console.log('Bad')
+                  n++
+                }
               }
-              else {
-                console.log('Bad')
-                n++
-              }
+              j++;
             }
-            j++;
           }
-        }
         })
         }
 
         let func2 = () => {request.get(url2).end((err, res) => {
-        P2[res.body.participant.id] = res.body.participant.name
-        const keyarray = Object.keys(P2)
-        var j = 0
-        if (keyarray.length === matchLen) {
-          while(matchLen > j) {
-            var n = 0
-            while(matchLen > n) {
-              if(infomation[j].player2Id.toString() === keyarray[n]) {
-                console.log('N2OK')
-                infomation[j].player2Name = P2[keyarray[n]]
-                break
+          P2[res.body.participant.id] = res.body.participant.name
+          const keyarray = Object.keys(P2)
+          var j = 0
+          if (keyarray.length === matchLen) {
+            while(matchLen > j) {
+              var n = 0
+              while(matchLen > n) {
+                if(infomation[j].player2Id.toString() === keyarray[n]) {
+                  console.log('N2OK')
+                  infomation[j].player2Name = P2[keyarray[n]]
+                  break
+                }
+                else {
+                  console.log('N2Bad')
+                  n++
+                }
               }
-              else {
-                console.log('N2Bad')
-                n++
+              j++;
+              if (j === matchLen) {
+                setName(infomation)
               }
-            }
-            j++;
-            if (j === matchLen) {
-              setName(infomation)
             }
           }
-        }
         })
         }
 
         await func1()
         func2()
-      
+
       }
       main()
     }
   }, [infomation]) 
 
-    return (
-      <>
-        <iframe title={EID} src={url} width='100%' height='500' frameborder='0' scrolling='auto' allowtransparency='true' />
+  return (
+    <>
+      <iframe title={EID} src={url} width='100%' height='500' frameborder='0' scrolling='auto' allowtransparency='true' />
 
-        <ul>
-          {infoandname.map(info =>
-            <li>{info.round}回戦 第{info.suggestedPlayOrder}試合{info.player1Name} vs {info.player2Name}</li>
+      <ul>
+        {infoandname.map(info =>
+          <li>{info.round}回戦 第{info.suggestedPlayOrder}試合{info.player1Name} vs {info.player2Name}</li>
 
-          )}
-        </ul>
-      </>
-    )
-  }
+        )}
+      </ul>
+    </>
+  )
+}
