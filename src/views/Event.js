f3d3e444981ff2e2b90f95f30088d246288eba37
n18008 <n18008@std.it-college.ac.jp>
2019-10-30 02:08:08 +0900

    New Event.js

--- src/views/Event.js
+++ src/views/Event.js
@@ -24,49 +24,77 @@ export default ({ match }) => {
   }, [])
 
   useMemo(() => {
-    const P1 = []
-    const P2 = []
+    const P1 = {}
+    const P2 = {} 
     const matchLen = Object.keys(infomation).length
-    for (var i = 0; matchLen > i; i++) {
+
+    for(var i = 0; matchLen > i; i++) {
       const url = `https://asia-northeast1-graduation-task-d7fc3.cloudfunctions.net/api/tournaments/n18011no5/participants/` + infomation[i].player1Id
-      
       const url2 = `https://asia-northeast1-graduation-task-d7fc3.cloudfunctions.net/api/tournaments/n18011no5/participants/` + infomation[i].player2Id
 
       request.get(url).end((err, res) => {
-        P1.push(res.body.participant.name)
-        if (P1.length === matchLen) {
-          for (var j = 0; matchLen > j; j++) {
-            infomation[j].player1Name = P1[j]
-            console.log(P1)
+        P1[res.body.participant.id] = res.body.participant.name
+        const keyarray = Object.keys(P1)
+        var j = 0
+        if (keyarray.length === matchLen) {
+          while(matchLen > j) {
+            var n = 0
+            while(matchLen > n) {
+              console.log(infomation[j].player1Id)
+              console.log(keyarray[n])
+              if (infomation[j].player1Id.toString() === keyarray[n]) {
+                console.log('OK')
+                infomation[j].player1Name = P1[keyarray[n]]
+                break
+              }
+              else {
+                console.log('Bad')
+                n++
+              }
+            }
+            j++;
           }
         }
       })
 
       request.get(url2).end((err, res) => {
-        P2.push(res.body.participant.name)
-        if (P2.length === matchLen) {
-          for (var j = 0; matchLen > j; j++) {
-            infomation[j].player2Name = P2[matchLen-1-j]
-            console.log(P2)
-            if (j === matchLen-1) {
+        P2[res.body.participant.id] = res.body.participant.name
+        const keyarray = Object.keys(P2)
+        var j = 0
+        if (keyarray.length === matchLen) {
+          while(matchLen > j) {
+            var n = 0
+            while(matchLen > n) {
+              if(infomation[j].player2Id.toString() === keyarray[n]) {
+                console.log('N2OK')
+                infomation[j].player2Name = P2[keyarray[n]]
+                break
+              }
+              else {
+                console.log('N2Bad')
+                n++
+              }
+            }
+            j++;
+            if (j === matchLen) {
               setName(infomation)
             }
-          } 
+          }
         }
       })
     }
-  }, [infomation])
+  }, [infomation]) 
 
-  return (
-    <>
-      <iframe title={EID} src={url} width='100%' height='500' frameborder='0' scrolling='auto' allowtransparency='true' />
+    return (
+      <>
+        <iframe title={EID} src={url} width='100%' height='500' frameborder='0' scrolling='auto' allowtransparency='true' />
 
-      <ul>
-        {infoandname.map(info =>
-          <li>{info.round}回戦 第{info.suggestedPlayOrder}試合{info.player1Name} vs {info.player2Name}</li>
+        <ul>
+          {infoandname.map(info =>
+            <li>{info.round}回戦 第{info.suggestedPlayOrder}試合{info.player1Name} vs {info.player2Name}</li>
 
-        )}
-      </ul>
-    </>
-  )
-}
+          )}
+        </ul>
+      </>
+    )
+  }
