c55fadf4b1e4a7c8997bf8884c77a5ec2233f9b7
n18011 <n18011@std.it-college.ac.jp>
2019-10-30 16:50:49 +0900

    result split becase for work

---
+++ src/components/ResultForm.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect, useMemo } from 'react'
+
+import {
+  Grid,
+  Paper,
+  Typography,
+  TextField
+} from '@material-ui/core'
+
+import { makeStyles } from '@material-ui/core/styles'
+import request from 'superagent'
+
+import { db } from '../Firebase'
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    marginLeft: theme.spacing(3),
+    marginBottom: theme.spacing(3)
+  },
+  textField: {
+    marginLeft: theme.spacing(3)
+  },
+  text: {
+    marginTop: theme.spacing(2)
+  }
+}))
+// VS以外の全てのTypography部にデータが入る
+export default ({ players }) => {
+  const classes = useStyles()
+  const values = ['', '', '', '', '']
+  return (
+    <>
+      <Grid container alignItems='center' justify='center'>
+
+        <Grid item xs container direction='column'>
+
+          <Paper>
+
+            <Grid item xs>
+              <Typography variant='h5' align='center' className={classes.text}>
+                {players.player1}
+              </Typography>
+            </Grid>
+
+            <Grid item xs>
+              {values.map((product, index) => (
+                <>
+
+                  <TextField
+                    id='filled-number'
+                    label={index + 1}
+                    value={product.player1}
+                    type='number'
+                    className={classes.textField}
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                    margin='normal'
+                  />
+                </>
+              ))
+              }
+            </Grid>
+
+            <Grid item xs>
+              <Typography variant='h5' align='center' gutterBottom>0</Typography>
+            </Grid>
+
+          </Paper>
+        </Grid>
+
+        <Grid item xs={2}>
+          <Typography variant='h4' align='center'>VS</Typography>
+        </Grid>
+
+        <Grid item xs container direction='column'>
+          <Paper>
+
+            <Grid item xs>
+              <Typography variant='h5' align='center' className={classes.text}>
+                {players.player2}
+              </Typography>
+            </Grid>
+
+            <Grid item container>
+              {values.map((product, index) => (
+                <>
+
+                  <TextField
+                    id='filled-number'
+                    label={index + 1}
+                    value={product.player2}
+                    type='number'
+                    className={classes.textField}
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                    margin='normal'
+                  />
+                </>
+              ))
+              }
+            </Grid>
+
+            <Grid item xs>
+              <Typography variant='h5' align='center' gutterBottom>0</Typography>
+            </Grid>
+
+          </Paper>
+        </Grid>
+
+      </Grid>
+    </>
+  )
+}
--- src/views/Result.js
+++ src/views/Result.js
@@ -1,32 +1,15 @@
 import React, { useState, useEffect, useMemo } from 'react'
 
-import {
-  Grid,
-  Paper,
-  Typography,
-  TextField
-} from '@material-ui/core'
-
-import { makeStyles } from '@material-ui/core/styles'
 import request from 'superagent'
 
 import { db } from '../Firebase'
 
-const useStyles = makeStyles(theme => ({
-  card: {
-    marginLeft: theme.spacing(3),
-    marginBottom: theme.spacing(3)
-  },
-  textField: {
-    marginLeft: theme.spacing(3)
-  },
-  text: {
-    marginTop: theme.spacing(2)
-  }
-}))
-// VS以外の全てのTypography部にデータが入る
+import ResultView from '../components/ResultView'
+import ResultForm from '../components/ResultForm'
+
 export default () => {
-  const classes = useStyles()
+  const [isProgresed, setIsProgresed] = useState(false)
+
   const [players, setPlayers] = useState({})
   const [values, setValues] = useState([])
   const [scoreCountP1, setScoreCountP1] = useState(0)
@@ -72,88 +55,24 @@ export default () => {
     setValues({ ...values, [name]: event.target.value })
   }
   */
+  useMemo(() => {
+    const eventRef = db.collection('events').doc('E001')
+    eventRef.collection('matchs').doc('M001').get()
+      .then(doc => {
+        setIsProgresed(doc.data().match_status.progresed)
+      })
+  }, [])
+
   return (
     <>
-      <Grid container alignItems='center' justify='center'>
-
-        <Grid item xs container direction='column'>
-
-          <Paper>
-
-            <Grid item xs>
-              <Typography variant='h5' align='center' className={classes.text}>
-                {players.player1}
-              </Typography>
-            </Grid>
-
-            <Grid item xs>
-              {values.map((product, index) => (
-                <>
-
-                  <TextField
-                    id='filled-number'
-                    label={index + 1}
-                    value={product.player1}
-                    type='number'
-                    className={classes.textField}
-                    InputLabelProps={{
-                      shrink: true
-                    }}
-                    margin='normal'
-                  />
-                </>
-              ))
-              }
-            </Grid>
-
-            <Grid item xs>
-              <Typography variant='h5' align='center' gutterBottom>{scoreCountP1}</Typography>
-            </Grid>
-
-          </Paper>
-        </Grid>
-
-        <Grid item xs={2}>
-          <Typography variant='h4' align='center'>VS</Typography>
-        </Grid>
-
-        <Grid item xs container direction='column'>
-          <Paper>
-
-            <Grid item xs>
-              <Typography variant='h5' align='center' className={classes.text}>
-                {players.player2}
-              </Typography>
-            </Grid>
-
-            <Grid item container>
-              {values.map((product, index) => (
-                <>
-
-                  <TextField
-                    id='filled-number'
-                    label={index + 1}
-                    value={product.player2}
-                    type='number'
-                    className={classes.textField}
-                    InputLabelProps={{
-                      shrink: true
-                    }}
-                    margin='normal'
-                  />
-                </>
-              ))
-              }
-            </Grid>
-
-            <Grid item xs>
-              <Typography variant='h5' align='center' gutterBottom>{scoreCountP2}</Typography>
-            </Grid>
-
-          </Paper>
-        </Grid>
-
-      </Grid>
+      {isProgresed ? (
+        <ResultView
+          players={players}
+          scoreCountP1={scoreCountP1}
+          scoreCountP2={scoreCountP2}
+          values={values}
+        />
+      ) : <ResultForm players={players} />}
     </>
   )
 }
