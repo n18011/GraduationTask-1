c51e97e6a99f976a8d881c0f1f2324fceee5e08a
n18008 <n18008@std.it-college.ac.jp>
2019-11-06 13:59:41 +0900

    commit

--- src/components/ResultForm.js
+++ src/components/ResultForm.js
@@ -46,12 +46,14 @@ export default ({ eid, mid, players }) => {
       },
     ])
 
-  const url = `https://asia-northeast1-graduation-task-d7fc3.cloudfunctions.net/api/tournaments/n18011no5/matches/178670634`
-  request.get(url).end((err, res) => {
-    console.log(res.body['match'].player1Id)
-    setPlayerid({
-      player1Id : res.body['match'].player1Id,
-      player2Id : res.body['match'].player2Id
+  useEffect(() => {
+    const url = `https://asia-northeast1-graduation-task-d7fc3.cloudfunctions.net/api/tournaments/n18011no5/matches/178670634`
+    request.get(url).end((err, res) => {
+      console.log(res.body['match'].player1Id)
+      setPlayerid({
+        player1Id : res.body['match'].player1Id,
+        player2Id : res.body['match'].player2Id
+      })
     })
   })
 
